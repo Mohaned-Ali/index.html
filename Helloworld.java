4b2abe14fbd81150acd4299ac5a984ad451be70d
Mr-Hajay <hajayali052449@gmail.com>
2020-08-25 18:20:44 +0300

    first java program

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>waxaad ku dhufataa number</h1>
+
+        <script src="script.js">
+        </script>
+
+    </body>
+</html>
